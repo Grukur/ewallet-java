d57ae693c12102268f684fbf3b3c06d1295b7bff
Grukur <darold.trench@gmail.com>
2024-02-29 22:42:09 -0300

    fix footer and other things and add a video on readme

--- assets/js/deposits.js
+++ assets/js/deposits.js
@@ -14,12 +14,20 @@ $(document).ready(function () {
 
     if (name && lastName) {
         $('#username').text(`Bienvenido ${name} ${lastName}`);
-        $('#sign-in_link').addClass('d-none')
+        $('#sign-in_link-1, #sign-in_link-2').addClass('d-none')
         $('#account_link').removeClass('d-none')
-        $('#clear_local').removeClass('d-none')
+        $('#clear_local-1, #clear_local-2').removeClass('d-none')
     } else {
         console.log('No hay usuario');
     }
+
+    $('#clear_local-1, #clear_local-2').click(() => {
+        localStorage.clear();
+        $('#sign-in_link-1, #sign-in_link-2').addClass('d-block')
+        $('#account_link').removeClass('d-block')
+        $('#clear_local-1, #clear_local-2').removeClass('d-block')
+        window.location.href = '/index.html';
+    })  
 
     $('#clear_local').click(() => {
         localStorage.clear();
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,18 +4,18 @@ $(document).ready(function () {
 
     if (name && lastName) {
         $('#username').text(`Bienvenido ${name} ${lastName}`);
-        $('#sign-in_link').addClass('d-none')
+        $('#sign-in_link-1, #sign-in_link-2').addClass('d-none')
         $('#account_link').removeClass('d-none')
-        $('#clear_local').removeClass('d-none')
+        $('#clear_local-1, #clear_local-2').removeClass('d-none')
     } else {
         console.log('No hay usuario');
     }
 
-    $('#clear_local').click(() => {
+    $('#clear_local-1, #clear_local-2').click(() => {
         localStorage.clear();
-        $('#sign-in_link').addClass('d-block')
+        $('#sign-in_link-1, #sign-in_link-2').addClass('d-block')
         $('#account_link').removeClass('d-block')
-        $('#clear_local').removeClass('d-block')
+        $('#clear_local-1, #clear_local-2').removeClass('d-block')
         window.location.href = '/index.html';
     })    
 });
